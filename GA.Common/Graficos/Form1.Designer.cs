580979219df169e1ad9902db5c3d9f70a9e903a3
felipexy toledo castro <felipexy@hotmail.com>
2019-04-28 12:52:47 -0300

    Gráficos - Atualizados.

--- GA.Common/Graficos/Form1.Designer.cs
+++ GA.Common/Graficos/Form1.Designer.cs
@@ -79,7 +79,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "RUN";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.Click += new System.EventHandler(this.botaoExec);
             // 
             // textBox2
             // 
--- GA.Common/Graficos/Form1.cs
+++ GA.Common/Graficos/Form1.cs
@@ -19,35 +19,27 @@ namespace Graficos
 
         
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void botaoExec(object sender, EventArgs e)
         {
-            //int popMax = 200;
-            //double taxaMut = 0.1;
-            //double taxaCross = 0.8;
-            //int maxGeracoes = 20;
+                int popMax = Convert.ToInt32(textBox1.Text);
+                double taxaCross = Convert.ToDouble(textBox2.Text);
+                double taxaMut = Convert.ToDouble(textBox3.Text);               
+                int maxGeracoes = Convert.ToInt32(textBox4.Text);
 
-            int popMax = Convert.ToInt32(textBox1.Text);
-            double taxaMut = Convert.ToDouble(textBox2.Text);
-            double taxaCross = Convert.ToDouble(textBox3.Text);
-            int maxGeracoes = Convert.ToInt32(textBox4.Text);
+                var ob = new GA.Common.GA(popMax, taxaMut, taxaCross, maxGeracoes);
+                ob.executaGA();
 
-            var ob = new GA.Common.GA(popMax, taxaMut, taxaCross, maxGeracoes);
-            ob.executaGA();
-
-            double[] arrayBestFit = new double[popMax];
-            double[] arrayAverage = new double[popMax];
+                double[] arrayBestFit = new double[popMax];
+                double[] arrayAverage = new double[popMax];
 
 
-            arrayBestFit = ob.bestArrayFit();
-            arrayAverage = ob.averageArrayFit();
+                arrayBestFit = ob.bestArrayFit();
+                arrayAverage = ob.averageArrayFit();
 
 
-            chart1.Series["Best"].Points.DataBindY(arrayBestFit);
-            chart1.Series["Average"].Points.DataBindY(arrayAverage);
-
-            arrayBestFit = null;
-            arrayAverage = null;
-
+                chart1.Series["Best"].Points.DataBindY(arrayBestFit);
+                chart1.Series["Average"].Points.DataBindY(arrayAverage);           
+            }           
         }
     }
-}
+
